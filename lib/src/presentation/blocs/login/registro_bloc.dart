6dbbbe52c542753192f88f22dbb2d1e00cfe019d
martin <ma.alvarado0309@gmail.com>
2020-11-28 07:36:59 -0500

    update

--- lib/src/presentation/blocs/login/registro_bloc.dart
+++ lib/src/presentation/blocs/login/registro_bloc.dart
@@ -33,12 +33,9 @@ class RegistroBloc extends MyBaseBloc {
   final Function(File) onChangedCamaraDeComercio;
   final Function(File) onChangedCedula;
   final Function(File) onChangedLogo;
-<<<<<<< HEAD
   final Function(bool) onChangedTerminosYcondiciones;
   final Function(bool) onChangedTerminosYcondicionesEmpresa;
   
-=======
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
 
   final ValueStream<String> nombre$;
   final ValueStream<String> apellido$;
@@ -47,7 +44,6 @@ class RegistroBloc extends MyBaseBloc {
   final ValueStream<String> contrasena$;
   final ValueStream<String> repetirContrasena$;
   final ValueStream<TipoRegistro> tipoRegistro$;
-<<<<<<< HEAD
   final ValueStream<bool> terminosYcondiciones$;
   final ValueStream<bool> terminosYcondicionesEmpresa$;
   
@@ -79,12 +75,6 @@ class RegistroBloc extends MyBaseBloc {
   
  
 
-=======
-
-  final Function cleanControllers;
-  final Stream<void> cleanControllers$;
-
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
   RegistroBloc._({
     this.onSubmit,
     this.isLoading$,
@@ -106,12 +96,9 @@ class RegistroBloc extends MyBaseBloc {
     this.onChangedCamaraDeComercio,
     this.onChangedCedula,
     this.onChangedLogo,
-<<<<<<< HEAD
     this.onChangedTerminosYcondiciones,
     this.onChangedTerminosYcondicionesEmpresa,
 
-=======
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
     this.nombre$,
     this.apellido$,
     this.email$,
@@ -119,12 +106,9 @@ class RegistroBloc extends MyBaseBloc {
     this.contrasena$,
     this.repetirContrasena$,
     this.tipoRegistro$,
-<<<<<<< HEAD
     this.terminosYcondiciones$,
     this.terminosYcondicionesEmpresa$,
 
-=======
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
     this.cleanControllers,
     this.cleanControllers$,
 
@@ -156,13 +140,9 @@ class RegistroBloc extends MyBaseBloc {
 
   factory RegistroBloc({
     UsuarioRepository usuarioRepository,
-<<<<<<< HEAD
     Validators validators
   }){
     
-=======
-  }) {
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
     //controllers
     final onSubmitController = PublishSubject<void>();
 
@@ -173,14 +153,9 @@ class RegistroBloc extends MyBaseBloc {
     final nroDocumentoController = BehaviorSubject<String>();
     final contrasenaController = BehaviorSubject<String>();
     final repetirContrasenaController = BehaviorSubject<String>();
-<<<<<<< HEAD
     final terminosYcondicionesController = BehaviorSubject<bool>.seeded(false);
     
     final tipoRegistroController = BehaviorSubject<TipoRegistro>.seeded(TipoRegistro.persona);
-=======
-    final tipoRegistroController =
-        BehaviorSubject<TipoRegistro>.seeded(TipoRegistro.persona);
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
 
     final razonSocialController = BehaviorSubject<String>();
     final nitController = BehaviorSubject<String>();
@@ -198,7 +173,6 @@ class RegistroBloc extends MyBaseBloc {
 
     //streams
     final registroMessage$ = onSubmitController
-<<<<<<< HEAD
       .exhaustMap((_) {
         if( tipoRegistroController.value == TipoRegistro.persona ) {
           return _onSubmitForm(
@@ -230,38 +204,6 @@ class RegistroBloc extends MyBaseBloc {
       })
       .share()
       .publish();
-=======
-        .exhaustMap((_) {
-          if (tipoRegistroController.value == TipoRegistro.persona) {
-            return _onSubmitForm(
-                usuarioRepository: usuarioRepository,
-                isLoadingSink$: isLoadingController.sink,
-                nombre: nombreController.value,
-                apellido: apellidoController.value,
-                email: emailController.value,
-                nroDocumento: nroDocumentoController.value,
-                contrasena: contrasenaController.value,
-                repetirContrasena: repetirContrasenaController.value);
-          } else {
-            return _onSubmitFormEmpresa(
-                usuarioRepository: usuarioRepository,
-                isLoadingSink$: isLoadingController.sink,
-                razonSocial: razonSocialController.value,
-                nit: nitController.value,
-                correoEmpresa: correoEmpresaController.value,
-                contrasenaEmpresa: contrasenaEmpresaController.value,
-                confirmarContrasenaEmpresa:
-                    repetirContrasenaEmpresaController.value,
-                codigoDeVendedor: codigoDeVendedorController.value,
-                rut: rutController.value,
-                camaraDeComercio: camaraDeComercioController.value,
-                cedula: cedulaController.value,
-                logo: logoController.value);
-          }
-        })
-        .share()
-        .publish();
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
     final nombre$ = nombreController.shareValue();
     final apellido$ = apellidoController.shareValue();
     final email$ = emailController.shareValue();
@@ -271,7 +213,6 @@ class RegistroBloc extends MyBaseBloc {
     final tipoRegistro$ = tipoRegistroController.distinct().shareValue();
     final cleanControllers$ = cleanControllersController.share();
 
-<<<<<<< HEAD
     final razonSocial$ = razonSocialController.shareValue();
     final nit$ = nitController.shareValue();
     final correoEmpresa$ = correoEmpresaController.shareValue();
@@ -301,8 +242,6 @@ class RegistroBloc extends MyBaseBloc {
     final isValidContrasenaEmpresa$ = contrasenaEmpresa$.transform(validators.validatorContrasena);
     final isValidCodigoDeVendedor$ = codigoDeVendedor$.transform(validators.validatorCodigoVendedor);
     
-=======
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
     //subscriptions
     final subscriptions = <StreamSubscription>[
       nombre$.listen((value) => print('[REGISTRO_BLOC] nombre=$value')),
@@ -311,7 +250,6 @@ class RegistroBloc extends MyBaseBloc {
       nroDocumento$
           .listen((value) => print('[REGISTRO_BLOC] nroDocumento=$value')),
       contrasena$.listen((value) => print('[REGISTRO_BLOC] contrasena=$value')),
-<<<<<<< HEAD
       repetirContrasena$.listen((value) => print('[REGISTRO_BLOC] repetirContrasena=$value')),
       tipoRegistro$.listen((value) => print('[REGISTRO_BLOC] tipoRegistro=$value')),
       terminosYcondiciones$.listen((value) => print('[REGISTRO_BLOC] terminosYcondiciones=$value')),
@@ -341,16 +279,6 @@ class RegistroBloc extends MyBaseBloc {
       isValidContrasenaEmpresa$.listen((value) => print('[REGISTRO_BLOC] isValidContrasenaEmpresa=$value')),
       isValidCodigoDeVendedor$.listen((value) => print('[REGISTRO_BLOC] isValidCodigoDeVendedor=$value')),
       
-=======
-      repetirContrasena$
-          .listen((value) => print('[REGISTRO_BLOC] repetirContrasena=$value')),
-      tipoRegistro$
-          .listen((value) => print('[REGISTRO_BLOC] tipoRegistro=$value')),
-      registroMessage$.listen(
-          (message) => print('[REGISTRO_BLOC] registroMessage=$message')),
-      cleanControllers$
-          .listen((_) => print('[REGISTRO_BLOC] cleanControllers executed')),
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
       registroMessage$.connect(),
     ];
 
@@ -385,7 +313,6 @@ class RegistroBloc extends MyBaseBloc {
     };
 
     return RegistroBloc._(
-<<<<<<< HEAD
       dispose: dispose,
       onSubmit: () => onSubmitController.add(null),
       isLoading$: isLoadingController.stream,
@@ -448,39 +375,6 @@ class RegistroBloc extends MyBaseBloc {
       isValidCodigoDeVendedor$: isValidCodigoDeVendedor$,
     );
 
-=======
-        dispose: dispose,
-        onSubmit: () => onSubmitController.add(null),
-        isLoading$: isLoadingController.stream,
-        registroMessage$: registroMessage$,
-        onChangedNombre: nombreController.sink.add,
-        onChangedApellido: apellidoController.sink.add,
-        onChangedEmail: emailController.sink.add,
-        onChangedNroDocumento: nroDocumentoController.sink.add,
-        onChangedContrasena: contrasenaController.sink.add,
-        onChangedRepetirContrasena: repetirContrasenaController.sink.add,
-        onChangedTipoRegistro: tipoRegistroController.sink.add,
-        onChangedRazonSocial: razonSocialController.sink.add,
-        onChangedNit: nitController.sink.add,
-        onChangedCorreoEmpresa: correoEmpresaController.sink.add,
-        onChangedContrasenaEmpresa: contrasenaEmpresaController.sink.add,
-        onChangedRepetirContrasenaEmpresa:
-            repetirContrasenaEmpresaController.sink.add,
-        onChangedcodigoDeVendedor: codigoDeVendedorController.sink.add,
-        onChangedRUT: rutController.sink.add,
-        onChangedCamaraDeComercio: camaraDeComercioController.sink.add,
-        onChangedCedula: cedulaController.sink.add,
-        onChangedLogo: logoController.sink.add,
-        nombre$: nombre$,
-        apellido$: apellido$,
-        email$: email$,
-        nroDocumento$: nroDocumento$,
-        contrasena$: contrasena$,
-        repetirContrasena$: repetirContrasena$,
-        tipoRegistro$: tipoRegistro$,
-        cleanControllers: () => cleanControllersController.add(null),
-        cleanControllers$: cleanControllers$);
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
   }
 
   //static method
@@ -518,7 +412,6 @@ class RegistroBloc extends MyBaseBloc {
   }
 
   //static method
-<<<<<<< HEAD
   static Stream<RegistroMessage> _onSubmitFormEmpresa({
     UsuarioRepository usuarioRepository,
     Sink<bool> isLoadingSink$,
@@ -535,22 +428,6 @@ class RegistroBloc extends MyBaseBloc {
     bool terminosYcondiciones
   }) async* {
     try{
-=======
-  static Stream<RegistroMessage> _onSubmitFormEmpresa(
-      {UsuarioRepository usuarioRepository,
-      Sink<bool> isLoadingSink$,
-      String razonSocial,
-      String nit,
-      String correoEmpresa,
-      String contrasenaEmpresa,
-      String confirmarContrasenaEmpresa,
-      String codigoDeVendedor,
-      File rut,
-      File camaraDeComercio,
-      File cedula,
-      File logo}) async* {
-    try {
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
       isLoadingSink$.add(true);
       final resp = await usuarioRepository.registroEmpresa(
         razonSocial: razonSocial,
@@ -591,7 +468,6 @@ class RegistroBloc extends MyBaseBloc {
   }
 
   //Functions
-<<<<<<< HEAD
   void cleanInputsText(){
     onChangedNombre(null);
     onChangedApellido(null);
@@ -612,15 +488,6 @@ class RegistroBloc extends MyBaseBloc {
     onChangedCedula(null);
     onChangedLogo(null);
     onChangedTerminosYcondicionesEmpresa(false);
-=======
-  void cleanInputsText() {
-    onChangedNombre('');
-    onChangedApellido('');
-    onChangedEmail('');
-    onChangedNroDocumento('');
-    onChangedContrasena('');
-    onChangedRepetirContrasena('');
->>>>>>> d85b15ece6c0f28f2fdf937ccf08f1a9a14e1e25
 
     // clean textEditingsControllers
     cleanControllers();
